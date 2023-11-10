8a5dc6717f4fd0a522aad36ac1fb3df34decbabf
disha <ddbulsara081222@gmail.com>
2023-11-10 18:25:10 +0530

    done

---
+++ Javascript/JavaScript Assignment/mathoperation.js
@@ -0,0 +1,21 @@
+function mathOperation() {
+    var num1 = document.getElementById("fno").value;  
+    var num2 = document.getElementById("sno").value;
+    
+    if ((num1 == null || num1 == "")|| (num2 == null || num2 == "")) {
+        alert("Enter Number")
+        
+    }
+}
+
+//Addition
+
+function Addval() {
+    var num1 = document.getElementById("fno").value;  
+    var num2 = document.getElementById("sno").value;
+    
+    if ((num1 == null || num1 == "")|| (num2 == null || num2 == "")) {
+        alert("Enter Number")
+        
+    }
+}
