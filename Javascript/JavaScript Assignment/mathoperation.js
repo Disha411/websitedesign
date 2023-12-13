05d365dc0c83196657a67c2cb6a8b51f77400855
disha <ddbulsara081222@gmail.com>
2023-12-13 13:12:56 +0530

    done

--- Javascript/JavaScript Assignment/mathoperation.js
+++ Javascript/JavaScript Assignment/mathoperation.js
@@ -1,21 +1,55 @@
-function mathOperation() {
-    var num1 = document.getElementById("fno").value;  
-    var num2 = document.getElementById("sno").value;
-    
-    if ((num1 == null || num1 == "")|| (num2 == null || num2 == "")) {
-        alert("Enter Number")
-        
-    }
-}
-
 //Addition
 
 function Addval() {
-    var num1 = document.getElementById("fno").value;  
-    var num2 = document.getElementById("sno").value;
+    var num1 = Number(document.getElementById("fno").value);  
+    var num2 = Number(document.getElementById("sno").value);
     
-    if ((num1 == null || num1 == "")|| (num2 == null || num2 == "")) {
-        alert("Enter Number")
-        
-    }
+    document.querySelector(".Answer").innerHTML = num1 + num2;    
+    // (num1 + num2) = document.querySelector(".Answer").innerHTML ;    
+
+    // document.getElementById(".fno").innerHTML="" 
+    // document.getElementById(".sno").innerHTML="" 
+    return false 
 }
+
+//Substraction
+
+function Subval() {
+    var num1 = Number(document.getElementById("fno").value);  
+    var num2 = Number(document.getElementById("sno").value);
+    
+    document.querySelector(".Answer").innerHTML = num1 - num2;
+    return false 
+}
+
+
+//Multiplication
+
+function Mulval() {
+    var num1 = Number(document.getElementById("fno").value);  
+    var num2 = Number(document.getElementById("sno").value);
+    
+    document.querySelector(".Answer").innerHTML = num1 * num2;
+    return false 
+}
+
+//Division
+
+function Divval() {
+    var num1 = Number(document.getElementById("fno").value);  
+    var num2 = Number(document.getElementById("sno").value);
+    
+    document.querySelector(".Answer").innerHTML = num1 / num2;
+    return false 
+}
+
+//Module
+
+function Modval() {
+    var num1 = Number(document.getElementById("fno").value);  
+    var num2 = Number(document.getElementById("sno").value);
+    
+    document.querySelector(".Answer").innerHTML = num1 % num2;
+    return false 
+}
+
