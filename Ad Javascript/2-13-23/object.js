55bd537270fb8aced70a982388236a58a0eab644
disha <ddbulsara081222@gmail.com>
2024-02-15 12:08:01 +0530

    done

---
+++ Ad Javascript/2-13-23/object.js
@@ -0,0 +1,37 @@
+// single thing multy details
+let person = {
+    name: "uv",
+    age: 23,
+  };
+
+  console.log(person);
+  
+ 
+ 
+ 
+  // let x =
+  let obj = {
+    str: "hello world",
+    num: 123,
+    nul: null,
+    und: undefined,
+    bol: true,
+    arr: [10, 20, 30, 40],
+    object: { city: "surat", pin: { a: 20, b: 50 } },
+    print: function (params) {
+      console.log("---print--->");
+    },
+  };
+  
+  let x = [1, 2, 3, 4];
+  console.log("-----------  x[2]----------->", x[2]);
+  
+  console.log(obj.str);
+  console.log(obj.und);
+  console.log(obj.object.city);
+  console.log(obj.object.pin.b);
+  console.log(obj.arr[2]);
+  
+  obj.print();
+  
+  
---
+++ Ad Javascript/2-13-23/task2.js
@@ -0,0 +1,13 @@
+let PersonalDetail = {
+    name : "Disha Deep Bulsara",
+    address : "Matru Chhaya,Pranami street , Chhipwad , Valsad - 396001 ",
+    age : 26,
+    hobby : ["Travelling", "Movie", "Chilling with friends"],
+  };
+  
+  console.log("my name is: "+ PersonalDetail.name);
+  console.log(PersonalDetail.address);
+  console.log(PersonalDetail.age);
+  console.log(PersonalDetail.hobby[1]);
+
+// store address , name ,age, hobby of yours
