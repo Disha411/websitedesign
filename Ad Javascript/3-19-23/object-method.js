a2ed4fe6f122b5f20a93d8fc8936b7cb0c1db88c
disha <ddbulsara081222@gmail.com>
2024-03-26 13:09:31 +0530

    done

--- Ad Javascript/3-19-23/object-method.js
+++ Ad Javascript/3-19-23/object-method.js
@@ -11,13 +11,13 @@ let obj = {
   // console.log("-----------  valueArr----------->", valueArr);
   
   let entries = Object.entries(obj);
-  // console.log("-----------  entries----------->", entries);
+  console.log("-----------  entries----------->", entries);
   
-  let user = {name:"disha"};
-  let kArr = Object.keys(user);
-  console.log("-----------  kArr----------->", kArr);
-  if (kArr.length === 0) {
-    console.log("user is empty");
-  } else {
-    console.log("user name is ", user.name);
-  }
+  // let user = {name:"disha"};
+  // let kArr = Object.keys(user);
+  // console.log("-----------  kArr----------->", kArr);
+  // if (kArr.length === 0) {
+  //   console.log("user is empty");
+  // } else {
+  //   console.log("user name is ", user.name);
+  // }
