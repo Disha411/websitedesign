5390e95ee29eb5789ae840826d4ae41f1e3f4196
Disha411 <ddbulsara081222@gmail.com>
2024-05-13 15:16:02 +0530

    done

--- React/task/src/6.Task6(input)/InputCom.jsx
+++ React/task/src/6.Task6(input)/InputCom.jsx
@@ -16,17 +16,31 @@ export default function InputCom() {
         setArr([...arr, task])
         setTask("")
     }
+    const DeleteHandler = (index) => {
+        let newArr = arr.filter((e, i) => i !== index);
+        setArr(newArr);
+    }
+    
     return (
         <div className="m-5 d-flex flex-column gap-2 align-items-center">
             <h2>Input : {task} </h2>
 
             <Input className="w-25" value={task} onChange={(e) => getInputData(e)} />
             <button onClick={() => addDataIntoArr()}>Add Task</button>
-                <div>
+            <div>
                 <ul>
-                    {arr.map((e) => {
-                        return <li>{e}<hr/></li>
+                    {arr.map((e, i) => {
+                        return (
+                            <>
+                                <div className="d-flex gap-5">
+                                    <li>{e}</li>
+                                    <button className="border border-1 " onClick={() => DeleteHandler(i)}>Delete</button>
+                                </div>
+                                <hr />
+                            </>
+                        );
                     })}
+
                 </ul>
             </div>
         </div>
