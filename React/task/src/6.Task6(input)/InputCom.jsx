75610ebca453bc8eba8d00b92245158dcb3fd06a
Disha411 <ddbulsara081222@gmail.com>
2024-05-13 14:42:10 +0530

    done

--- React/task/src/6.Task6(input)/InputCom.jsx
+++ React/task/src/6.Task6(input)/InputCom.jsx
@@ -1,29 +1,31 @@
 import React, { useState } from "react";
-// import {input} from "reactstrap";
+import { Input } from "reactstrap";
+
 
 export default function InputCom() {
-    let [task,setTask] = useState("")
-    let [arr,setArr] = useState([])
-    
+    let [task, setTask] = useState("")
+    let [arr, setArr] = useState([])
+
     const getInputData = (e) => {
         console.log(e.target.value) //e.target.value bydefault
         setTask(e.target.value)
     }
-    const addDataIntoArr =()=>{
+    const addDataIntoArr = () => {
 
         console.log(addDataIntoArr)
-        setArr([...arr,task])
+        setArr([...arr, task])
         setTask("")
     }
     return (
-        <div >
+        <div className="m-5 d-flex flex-column gap-2 align-items-center">
             <h2>Input : {task} </h2>
-            <input value={task} onChange={(e) => getInputData(e)} />
-            <button onClick={()=>addDataIntoArr()}>Add Task</button>
-            <div>
+
+            <Input className="w-25" value={task} onChange={(e) => getInputData(e)} />
+            <button onClick={() => addDataIntoArr()}>Add Task</button>
+                <div>
                 <ul>
-                    {arr.map((e)=>{
-                        return<li>{e}</li>
+                    {arr.map((e) => {
+                        return <li>{e}<hr/></li>
                     })}
                 </ul>
             </div>
