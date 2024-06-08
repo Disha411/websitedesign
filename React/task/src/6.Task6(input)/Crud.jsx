09a83452342f503f725ffa568f6f76ee82168ff2
Disha411 <ddbulsara081222@gmail.com>
2024-06-08 19:37:22 +0530

    done

--- React/task/src/6.Task6(input)/Crud.jsx
+++ React/task/src/6.Task6(input)/Crud.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Button, Input, Table } from 'reactstrap'
 
 export default function Crud() {
-    let [data, setdata] = useState({ Name: "", Email: "", gender: "" })
+    let [data, setdata] = useState({ Name: "", Email: "", gender: "", hobby: [] })
     let [Arr, setArr] = useState([])
 
     // let [index, setIndex] = useState(null)
@@ -15,24 +15,33 @@ export default function Crud() {
         let newarr = Arr.filter((e, i) => i !== index)
         setArr(newarr)
     }
+    const checkBoxhandler = (value, e) => {
+        if (e.target.checked) {
+            setdata([...data, value])
+        } else {
+            let filterData = data.hobby.filter((e) => e !== value)
+            setdata(filterData)
+        }
+    }
 
     return (
         <div className='m-5 p-5 w-3 h-4 '>
-            <div className='d-flex flex-column gap-3'>
+            <div className='inputs d-flex flex-column gap-3'>
                 <Input value={data.Name} placeholder='Enter Name' onChange={(e) => setdata({ ...data, Name: e.target.value })} />
                 <Input value={data.Email} placeholder='Enter Email' onChange={(e) => setdata({ ...data, Email: e.target.value })} />
             </div>
 
-            <div className='m-2'>
-                <Input type="radio" checked={gender === "male"} onChange={() => setdata({ ...data, gender: ("male") })} />Male
-                <Input type="radio" checked={gender === "female"} onChange={() => setdata({ ...data, gender: ("female") })} />Female
-                <Input type='radio' checked={gender === "other"} onChange={() => setdata({ ...data, gender: ("other") })} />Other
+            <div className='radiobuttons m-2'>
+                <Input type="radio" checked={data.gender === "male"} onChange={() => setdata({ ...data, gender: ("male") })} />Male
+                <Input type="radio" checked={data.gender === "female"} onChange={() => setdata({ ...data, gender: ("female") })} />Female
+                <Input type='radio' checked={data.gender === "other"} onChange={() => setdata({ ...data, gender: ("other") })} />Other
             </div>
-            <div>
-                <Input type="checkbox" />Cricket
-                <Input type="checkbox" />Vollyball
-                <Input type="checkbox" />Dancing
-                <Input type="checkbox" />Singing
+
+            <div className='hobbycheckboxes'>
+                <Input type="checkbox" checked={data.hobby.includes("Cricket")} onChange={(e) => checkBoxhandler("Cricket", e)} />Cricket
+                <Input type="checkbox" checked={data.hobby.includes("Vollyball")} onChange={(e) => checkBoxhandler("Vollyball", e)} />Vollyball
+                <Input type="checkbox" checked={data.hobby.includes("Dancing")} onChange={(e) => checkBoxhandler("Dancing", e)} />Dancing
+                <Input type="checkbox" checked={data.hobby.includes("Singing")} onChange={(e) => checkBoxhandler("Singing", e)} />Singing
             </div>
             <div className=''>
                 <select>
