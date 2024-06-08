1330ccc6fe13fa47c08e32a0ed94fab01de33914
Disha411 <ddbulsara081222@gmail.com>
2024-06-08 19:12:22 +0530

    Done

--- React/task/src/6.Task6(input)/Crud.jsx
+++ React/task/src/6.Task6(input)/Crud.jsx
@@ -2,13 +2,14 @@ import React, { useState } from 'react'
 import { Button, Input, Table } from 'reactstrap'
 
 export default function Crud() {
-    let [data, setdata] = useState({ Name: "", Email: "", gender: "", hobby: "" })
+    let [data, setdata] = useState({ Name: "", Email: "", gender: "" })
     let [Arr, setArr] = useState([])
+
     // let [index, setIndex] = useState(null)
 
     const submithandler = (e) => {
         setArr([...Arr, data])
-        setdata({ Name: "", Email: "", gender: "", hobby: "" })
+        setdata({ Name: "", Email: "", gender: "" })
     }
     const DeleteHandler = (index) => {
         let newarr = Arr.filter((e, i) => i !== index)
@@ -23,9 +24,9 @@ export default function Crud() {
             </div>
 
             <div className='m-2'>
-                <Input type="radio" checked={data.gender === "male"} onChange={() => setdata({ ...data, gender: ("male") })} />Male
-                <Input type="radio" checked={data.gender === "female"} onChange={() => setdata({ ...data, gender: ("female") })} />Female
-                <Input type='radio' checked={data.gender === "other"} onChange={() => setdata({ ...data, gender: ("other") })} />Other
+                <Input type="radio" checked={gender === "male"} onChange={() => setdata({ ...data, gender: ("male") })} />Male
+                <Input type="radio" checked={gender === "female"} onChange={() => setdata({ ...data, gender: ("female") })} />Female
+                <Input type='radio' checked={gender === "other"} onChange={() => setdata({ ...data, gender: ("other") })} />Other
             </div>
             <div>
                 <Input type="checkbox" />Cricket
@@ -34,12 +35,12 @@ export default function Crud() {
                 <Input type="checkbox" />Singing
             </div>
             <div className=''>
-                {/* <select>
+                <select>
                     <option value="">Admin</option>
                     <option value="">HR</option>
                     <option value="">QC</option>
                     <option value="">Production</option>
-                </select> */}
+                </select>
             </div>
             <div className='text-center'>
                 <Button className='w-2 bg-success m-1' onClick={(e) => submithandler(e)}>Submit</Button>
@@ -61,7 +62,7 @@ export default function Crud() {
                                 <th scope="row">{i + 1}</th>
                                 <td>{e.Name}</td>
                                 <td>{e.Email}</td>
-                                <td>{e.gender}</td>
+                                <td>{data.gender}</td>
                                 <td></td>
                                 <td><Button>Update</Button></td>
                                 <td><Button className='bg-danger' onClick={() => DeleteHandler(i)}>Delete</Button></td>
