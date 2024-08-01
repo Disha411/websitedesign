1da153d6ccce5de60d541372e239ef9ece2d958b
Disha411 <ddbulsara081222@gmail.com>
2024-08-01 12:35:27 +0530

    done

--- React/disha project/src/ui/page/Adminpages/Product.jsx
+++ React/disha project/src/ui/page/Adminpages/Product.jsx
@@ -6,6 +6,27 @@ import ReactPaginate from "react-paginate";
 import { useNavigate } from "react-router-dom";
 import ReactSelect from "react-select";
 import { Button, Input, Modal, ModalBody, ModalFooter, ModalHeader, Table } from "reactstrap";
+import Select from "react-select";
+
+const maincategoryselecthandler = [
+  { value: "tshirt", label: "tshirt" },
+  { value: "shirt", label: "shirt" },
+  { value: "jeans", label: "jeans" },
+  { value: "trouser", label: "trouser" },
+];
+
+const alldata = {
+  title: "",
+  description: "",
+  size: "",
+  color: [],
+  mainCategorie: "",
+  price: "",
+  image: "",
+  stock: "",
+  discountPercentage: "",
+};
+
 
 export default function Product() {
   let [flag, setFlag] = useState(true);
@@ -16,14 +37,58 @@ export default function Product() {
   let [modal, setModal] = useState(false);
   let [searchText, setsearchText] = useState("")
 
-  const toggle = () => setModal(!modal);
+  let [data, setdata] = useState(alldata);
+  let [color, setcolor] = useState([])
+  let [size, setsize] = useState([])
 
-  console.log("-----------  count----------->", count);
+  const navigate = useNavigate();
+
+  const sizecheckBoxHandler = (value, e) => {
+    if (e.target.checked) {
+      setsize([...size, value]);
+    } else {
+      let filterData = size.filter((e) => e !== value);
+      setsize(filterData);
+    }
+  };
+
+  const colorcheckBoxHandler = (value, e) => {
+    if (e.target.checked) {
+      setcolor([...color, value]);
+    } else {
+      let filterData = color.filter((e) => e !== value);
+      setcolor(filterData);
+    }
+  };
+
+  const Updatehandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      let response = await axios.post(
+        "http://localhost:9999/product/create",
+        data
+      );
+      setdata(alldata);
+      navigate("/product");
+    } catch (error) {
+      console.log("-----------  error----------->", error);
+    }
+    console.log("🚀 ~ Login ~ data:", data);
+  };
+
+  const toggle = () => setModal(!modal);
+  const editHandler = (e) => {
+    setdata(e)
+    setsize(e.size)
+    toggle()
+    setcolor(e.color)
+  }
 
   const refetch = () => setFlag(!flag);
 
   let [cookies] = useCookies(["token"]);
-  const navigate = useNavigate();
+
 
   const callApiHandler = async () => {
     try {
@@ -77,7 +142,7 @@ export default function Product() {
               { label: 25, value: 25 },
               { label: 50, value: 50 },
             ]}
-            onChange={(e) => (setLimit(e.value),setPage(1))}
+            onChange={(e) => (setLimit(e.value), setPage(1))}
           />
           <Button onClick={() => navigate("/addproduct-form")}>Add Product</Button>
         </div>
@@ -88,7 +153,7 @@ export default function Product() {
         className="flex gap-3 justify-center my-3"
         activeClassName="bg-black text-white"
         onPageChange={(e) => setPage(e.selected + 1)}
-        forcePage={page-1}
+        forcePage={page - 1}
         pageClassName="border py-2 px-3 border-black rounded-full"
       />
 
@@ -103,7 +168,7 @@ export default function Product() {
             <th className="w-[50px]">Price</th>
             <th>Images</th>
             <th className="w-[150px]">Thumbnail</th>
-            <th className="w-[25px]">Discount %</th>
+            <th className="w-[120px]">Discount %</th>
             <th className="w-[30px]">Category</th>
             <th>Color</th>
             <th>Size</th>
@@ -124,22 +189,166 @@ export default function Product() {
                     >
                       Delete
                     </p>
-                    <p className="cursor-pointer text-blue-500 underline" onClick={toggle}>
+                    <p className="cursor-pointer text-blue-500 underline" onClick={() => editHandler(e)}>
                       Edit
                     </p>
                     <Modal isOpen={modal} toggle={toggle}>
                       <ModalHeader toggle={toggle}>What you want to Edit</ModalHeader>
                       <ModalBody>
-                        Edit or not
+                        <div className="mx-3  text-gray">
+                          <h1 className="h1 text-center">Update Product</h1>
+
+                          <label>Title</label>
+                          <Input
+                            value={data.title}
+                            type="text"
+                            placeholder="Enter a title"
+                            className="bg-slate-100"
+                            onChange={(e) => setdata({ ...data, title: e.target?.value })}
+                          />
+
+                          <label>Description</label>
+                          <Input
+                            value={data.description}
+                            type="text"
+                            placeholder="Enter a Description"
+                            className="bg-slate-100"
+                            onChange={(e) => setdata({ ...data, description: e.target?.value })}
+                          />
+
+                          <label>size</label>
+                          <div className="flex gap-2">
+                          <Input
+                              value={size}
+                              type="checkbox"
+                              checked={size.includes("xs")}
+                              onChange={(e) => sizecheckBoxHandler("xs", e)}
+                            />
+                            XS
+                            <Input
+                              value={size}
+                              type="checkbox"
+                              checked={size.includes("s")}
+                              onChange={(e) => sizecheckBoxHandler("s", e)}
+                            />
+                            S
+                            <Input
+                              value={size}
+                              type="checkbox"
+                              checked={size.includes("m")}
+                              onChange={(e) => sizecheckBoxHandler("m", e)}
+                            />
+                            M
+                            <Input
+                              value={size}
+                              type="checkbox"
+                              checked={size.includes("l")}
+                              onChange={(e) => sizecheckBoxHandler("l", e)}
+                            />
+                            L
+                            <Input
+                              value={size}
+                              type="checkbox"
+                              checked={size.includes("xl")}
+                              onChange={(e) => sizecheckBoxHandler("xl", e)}
+                            />
+                            XL
+                            <Input
+                              value={size}
+                              type="checkbox"
+                              checked={size.includes("2xl")}
+                              onChange={(e) => sizecheckBoxHandler("2xl", e)}
+                            />
+                            2XL
+                          </div>
+
+                          <label>Color</label>
+                          <div className="flex gap-2">
+
+                            <Input
+                              value={color}
+                              type="checkbox"
+                              checked={color.includes("red")}
+                              onChange={(e) => colorcheckBoxHandler("red", e)}
+                            />red
+
+                            <Input
+                              value={color}
+                              type="checkbox"
+                              checked={color.includes("yellow")}
+                              onChange={(e) => colorcheckBoxHandler("yellow", e)}
+                            />yellow
+
+                            <Input
+                              value={color}
+                              type="checkbox"
+                              checked={color.includes("black")}
+                              onChange={(e) => colorcheckBoxHandler("black", e)}
+                            />black
+
+                            <Input
+                              value={color}
+                              type="checkbox"
+                              checked={color.includes("White")}
+                              onChange={(e) => colorcheckBoxHandler("White", e)}
+                            />White
+                          </div>
+
+                          <br />
+
+                          <label>Category</label>
+                          <Select
+                            value={{ value: data.mainCategorie, label: data.mainCategorie }}
+                            options={maincategoryselecthandler}
+                            onChange={(e) => setdata({ ...data, mainCategorie: e.value })}
+                          />
+
+                          <label>Price</label>
+                          <Input
+                            value={data.price}
+                            type="number"
+                            placeholder="Enter a Price"
+                            className="bg-slate-100"
+                            onChange={(e) => setdata({ ...data, price: e.target?.value })}
+                          />
+
+                          <label>Image</label>
+                          <Input
+                            value={data.image}
+                            type="text"
+                            placeholder="Enter a ImageURL"
+                            className="bg-slate-100"
+                            onChange={(e) => setdata({ ...data, image: e.target?.value })}
+                          />
+
+                          <label>Stock</label>
+                          <Input
+                            value={data.stock}
+                            type="text"
+                            placeholder="Enter a available stock"
+                            className="bg-slate-100"
+                            onChange={(e) => setdata({ ...data, stock: e.target?.value })}
+                          />
+
+                          <label>Discount</label>
+                          <Input
+                            value={data.discountPercentage}
+                            type="text"
+                            placeholder="Enter a discount"
+                            className="bg-slate-100"
+                            onChange={(e) => setdata({ ...data, discountPercentage: e.target?.value })}
+                          />
+
+                          <div className="text-center mt-3">
+                            <Button
+                              className="w-full border-2 border-red-600 text-red-600 bg-white hover:text-red-600 hover:border-red-600"
+                              onClick={(e) => Updatehandler(e)}
+                            >
+                              Update
+                            </Button>
+                          </div>
+                        </div>
                       </ModalBody>
-                      <ModalFooter>
-                        <Button color="primary" onClick={toggle}>
-                          Do Something edit
-                        </Button>{' '}
-                        <Button color="secondary" onClick={toggle}>
-                          Cancel
-                        </Button>
-                      </ModalFooter>
                     </Modal>
                   </div>
                 </td>
@@ -151,7 +360,9 @@ export default function Product() {
                   <img className="h-[50px]" src={e.image} alt="" srcset="" />
                 </td>
                 <td>
-                  <img className="w-[130px] h-[130px]" src={e.thumbnail} alt="" />
+                  <img className="w-[130px] h-[130px]" src={e?.thumbnail || "https://static.thenounproject.com/png/2932881-200.png"} alt="" onError={(e) => {
+                    e.target.src = "https://static.thenounproject.com/png/2932881-200.png"
+                  }} />
                 </td>
                 <td>{e.discountPercentage}</td>
                 <td>
